324fbef07c929a768550f18bcdb3b998bccd812a
Bhavesh Sood <bhaveshsood13@gmail.com>
2020-11-20 21:29:23 +0530

    mainmenu

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,12 +2,18 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-    final private int height = 1000 ;
+public class git Main extends Application {
+    final private int height = 700 ;
     final private double ratio = (9/16.0) ;
 
 
@@ -16,7 +22,6 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Game");
         primaryStage.setScene(new Scene(root, height*ratio, height));
-
         primaryStage.show();
     }
 
