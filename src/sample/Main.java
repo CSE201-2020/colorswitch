e4dbd1b95e792907324ce254ad480f381381b9c8
Bhavesh Sood <bhaveshsood13@gmail.com>
2020-11-22 16:09:33 +0530

    Login added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,7 +19,7 @@ public class  Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Game");
         primaryStage.setScene(new Scene(root, height*ratio, height));
         primaryStage.show();
